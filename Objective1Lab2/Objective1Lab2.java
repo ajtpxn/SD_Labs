f43173e84442df609e028d106edd108a7f0cae12
ajtpxn <ajtpxn@gmail.com>
2018-08-21 14:39:16 -0600

    First commit

---
+++ Objective1Lab2/Objective1Lab2.java
@@ -0,0 +1,6 @@
+public class Objective1Lab2 {
+  public static void main(String[] args){
+    System.out.println("Brandon Jaloway");
+    System.out.println("40");
+  }
+}
---
+++ Objective1Lab3/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main(String[] args){
+    System.out.println( "Name : Brandon Jaloway");
+    System.out.println( "Age : 40");
+    System.out.println( "State : Colorado");
+  }
+}
